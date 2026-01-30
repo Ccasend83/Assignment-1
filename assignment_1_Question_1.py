ccb0e305ce814a185a1e388d536183ed60a8cb3b
Ccasend83 <140461687+Ccasend83@users.noreply.github.com>
2026-01-30 16:17:42 -0700

    Update assignment_1_Question_1.py
    Comments were added

--- assignment_1_Question_1.py
+++ assignment_1_Question_1.py
@@ -1,9 +1,12 @@
-max_threshold_number = 100
-product_number = 1
+max_threshold_number = 100 # max_threshold_number is the number we want to become greater than in our loop
+product_number = 1 # both product number and multiplication number start out as normal ones but increase further
 multiplication_number = 1
 
-while max_threshold_number > product_number:
-    product_number = product_number * multiplication_number
-    multiplication_number += 1
-print("Final product " + str(product_number))
+while max_threshold_number > product_number: # while our threshold number is greater than the product number it will 
+    # continue to loop
+    product_number = product_number * multiplication_number # it multiplies the current product number by our multiplication number
+    # which updates the current value of product number
+    multiplication_number += 1 # increases the value of the multiplier number by 1 each time it loops
+    
+print("Final product " + str(product_number)) # lastly, it outputs the final product number and the current multiplication number
 print("Final current number: " + str(multiplication_number))
