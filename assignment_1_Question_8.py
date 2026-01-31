bd250b6e407e57f6a225cc8d57bea93b6123dd35
Ccasend83 <140461687+Ccasend83@users.noreply.github.com>
2026-01-30 20:09:23 -0700

    Update assignment_1_Question_8.py

--- assignment_1_Question_8.py
+++ assignment_1_Question_8.py
@@ -1,12 +1,12 @@
-import pandas as pd
+import pandas as pd # we import the pandas library as pd so whenever we refer to it we use pd
 
-initial_stored_column_data = {
+initial_stored_column_data = { # this is our initial data given by the assignment page
 "A": [1, 2, 2, 1],
 "B": [3.1, 4.2, 1.5, 6.3],
 "C": [800, 150, 400, 210]
 }
 
-new_panda_dataframe_for_storing_data = pd.DataFrame(initial_stored_column_data)
-new_panda_dataframe_for_storing_data["D"] = (new_panda_dataframe_for_storing_data["C"] /
+new_panda_dataframe_for_storing_data = pd.DataFrame(initial_stored_column_data) # firstly we create the dataframe using our initial stored data
+new_panda_dataframe_for_storing_data["D"] = (new_panda_dataframe_for_storing_data["C"] / # here we create a new row by dividing the contents of row "C" by the contents of row "B"
                                              new_panda_dataframe_for_storing_data["B"])
 print(new_panda_dataframe_for_storing_data)
